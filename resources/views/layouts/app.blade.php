f6d5c30741b60f5dd0e7afcfef9ec29b8fc2cd66
HugoSanchezv <l20030008@fresnillo.tecnm.mx>
2024-06-02 19:23:47 -0600

    add livewire component for likes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <script src="https://cdn.tailwindcss.com"></script>
+
+    @livewireStyles
 </head>
 
 <body class="bg-gray-100">
@@ -63,6 +65,7 @@
     <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
         DevStagram - Todos los derechos reservados {{ now()->year }}
     </footer>
+    @livewireScripts
 </body>
 
 </html>
